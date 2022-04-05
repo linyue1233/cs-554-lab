f08ed8b8d67c78c9a3f5c63cbb8e79d17d3d6f78
linyue1233 <1191609469@qq.com>
2022-04-04 23:48:20 -0400

    lab5 created

---
+++ lab5/server/config/mongoCollections.js
@@ -0,0 +1,18 @@
+const dbConnection = require('./mongoConnections');
+
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+module.exports = {
+  imagePosts:getCollectionFn('imagePosts')
+}
